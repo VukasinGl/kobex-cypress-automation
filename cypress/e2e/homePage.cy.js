6aad299fb735ae72e53d0ae927797929deeaca89
VukasinGl <vukasin.glogonjac@automaticity.rs>
2023-08-21 12:11:53 +0200

    Header and footer, and blanco drop-down menu functions added to the before hook

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -1,13 +1,16 @@
 /// <reference types="Cypress" />
+import "cypress-real-events/support";
+
 import { headerAndFooterBL } from "../modules/BL/headerAndFooterBL";
 import { homePageBL } from "../modules/BL/homePageBL";
-
+import { blancoDropDownMenuPOM } from "../modules/POM/headerAndFooterPOM/blancoDropDownMenuPOM";
 describe("Testing Home Page UI", () => {
   before(() => {
-    cy.visit("/");
+    cy.visit(`${Cypress.env("baseUrl")}`);
+    headerAndFooterBL.assertHeaderAndFooterElemets();
+    blancoDropDownMenuPOM.assertBlancoDropdownMenu();
   });
   it("Assert all the elements on the home page", () => {
-    headerAndFooterBL.assertGenericElemets();
     homePageBL.assertHomePageElements();
   });
 });
