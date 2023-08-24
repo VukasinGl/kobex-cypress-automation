0982e57fdf87fd94bc2e85b7f76c94c6857ba179
VukasinGl <vukasin.glogonjac@automaticity.rs>
2023-08-24 13:19:16 +0200

    Test data added

--- cypress/fixtures/testData.js
+++ cypress/fixtures/testData.js
@@ -1,1 +1,8 @@
-
+export const oNamaPageData = {
+  title: "O nama",
+  p1: "KOBEX je osnovan 2001. godine u Novom Sadu.",
+  p2: "Od svog osnivanja firma se prvenstveno bavi plasmanom proizvoda renomirane nemačke firme BLANCO, čiji je jedini uvoznik i generalni distributer za Srbiju.",
+  p3: "Pored toga, KOBEX se bavi plasmanom ugradne bele tehnike proizvodjača Electrolux i AEG.",
+  p4: "Sa više od dve decenije postojanja firma se etablirala na tržištu kao ekspert za kuhinjsku tehniku i kao pouzdan partner spreman da  adekvatno odgovori na sve zahteve.",
+};
+export const googleMapKobex = "https://maps.google.com/maps?q=loc:Kobex";
