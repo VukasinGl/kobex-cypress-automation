207cd5858be6b39927bb3918cd8753215ea94f44
VukasinGl <vukasin.glogonjac@automaticity.rs>
2023-08-28 10:51:57 +0200

    Spaces created

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -1,14 +1,15 @@
 /// <reference types="Cypress" />
+
 import { headerAndFooterBL } from "../modules/BL/headerAndFooterBL";
 import { homePageBL } from "../modules/BL/homePageBL";
 import { blancoDropDownMenuPOM } from "../modules/POM/headerAndFooterPOM/blancoDropDownMenuPOM";
 import { electroluxDropDownMenuPOM } from "../modules/POM/headerAndFooterPOM/electroluxDropDownMenuPOM";
 import { tekaDropDownMenuPOM } from "../modules/POM/headerAndFooterPOM/tekaDropDownMenuPOM";
 import { syskorDropDownMenuPOM } from "../modules/POM/headerAndFooterPOM/syskorDropDownMenuPOM";
+
 describe("Testing Home Page UI", () => {
   before(() => {
     cy.visit(`${Cypress.env("baseUrl")}`);
-    headerAndFooterBL.assertHeaderAndFooterElemets();
     blancoDropDownMenuPOM.assertBlancoDropdownMenu();
     electroluxDropDownMenuPOM.assertElectroluxDropDownMenu();
     tekaDropDownMenuPOM.assertTekaDropDownMenu();
@@ -16,5 +17,8 @@ describe("Testing Home Page UI", () => {
   });
   it("Assert all the elements on the home page", () => {
     homePageBL.assertHomePageElements();
+  });
+  after(() => {
+    headerAndFooterBL.assertHeaderAndFooterElemets();
   });
 });
