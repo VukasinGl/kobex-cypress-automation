fccc0942957be4895378201230a0380bb4e69235
VukasinGl <vukasin.glogonjac@automaticity.rs>
2023-08-29 11:25:11 +0200

    Business logic, getters and assertion for blanco pribor page

---
+++ cypress/e2e/blanco/blancoPriborPage.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+import { headerAndFooterBL } from "../../modules/BL/headerAndFooterBL";
+import { blancoDropDownMenuPOM } from "../../modules/POM/headerAndFooterPOM/blancoDropDownMenuPOM";
+import { electroluxDropDownMenuPOM } from "../../modules/POM/headerAndFooterPOM/electroluxDropDownMenuPOM";
+import { tekaDropDownMenuPOM } from "../../modules/POM/headerAndFooterPOM/tekaDropDownMenuPOM";
+import { syskorDropDownMenuPOM } from "../../modules/POM/headerAndFooterPOM/syskorDropDownMenuPOM";
+import { blancoPriborBL } from "../../modules/BL/blancoBL/blancoPriborPageBL";
+
+describe("Testing blanco priborr page UI", () => {
+  before(() => {
+    cy.visit(`${Cypress.env("baseUrl")}`);
+  });
+
+  it("Assert all the elements on the blanco pribor page", () => {
+    blancoPriborBL.assertBlancoPriborPage();
+  });
+
+  after(() => {
+    headerAndFooterBL.assertHeaderAndFooterElemets();
+    blancoDropDownMenuPOM.assertBlancoDropdownMenu();
+    electroluxDropDownMenuPOM.assertElectroluxDropDownMenu();
+    tekaDropDownMenuPOM.assertTekaDropDownMenu();
+    syskorDropDownMenuPOM.assertSyskorDropDownMenu();
+  });
+});
---
+++ cypress/modules/POM/blancoPOM/blancoPriborPagePOM.js
@@ -0,0 +1,39 @@
+class BlancoPriborPOM {
+  get title() {
+    return cy.get("h1").contains("BLANCO - BLANCO Pribor");
+  }
+  get artikliNaStanjuFilter() {
+    return cy.get("#stanje_div");
+  }
+  get stanjeCheckbox() {
+    return cy.get("input[id='stanje']");
+  }
+  get izdvajamoFilter() {
+    return cy.get("#izdvojeni_div");
+  }
+  get sortirajDropDownMenu() {
+    return cy.get("#view-option-sort");
+  }
+  get poNazivuA_Zoption() {
+    return cy.get("select").select("Po nazivu A-Z");
+  }
+  get poNazivuZ_Aoption() {
+    return cy.contains("Po nazivu Z-A");
+  }
+  get poCeniRastuceOption() {
+    return cy.contains("Po ceni rastuće");
+  }
+  get poCeniOpadajućeOption() {
+    return cy.contains("Po ceni opadajuće");
+  }
+  get poUnosuOpadajuceOption() {
+    return cy.contains("Po unosu opadajuće");
+  }
+  get ponistiBtn() {
+    return cy.contains("Poništi");
+  }
+  get filteri() {
+    return cy.get("form[id='filteri']");
+  }
+}
+export const blancoPriborPOM = new BlancoPriborPOM();
