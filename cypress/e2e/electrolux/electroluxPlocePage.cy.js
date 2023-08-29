d6821e6d0f90cf9489601d850d29d51463d28b12
VukasinGl <vukasin.glogonjac@automaticity.rs>
2023-08-29 15:09:34 +0200

    Business logic, getters and assertion for electrolux ploce page created

---
+++ cypress/e2e/electrolux/electroluxPlocePage.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+import { headerAndFooterBL } from "../../modules/BL/headerAndFooterBL";
+import { blancoDropDownMenuPOM } from "../../modules/POM/headerAndFooterPOM/blancoDropDownMenuPOM";
+import { electroluxDropDownMenuPOM } from "../../modules/POM/headerAndFooterPOM/electroluxDropDownMenuPOM";
+import { tekaDropDownMenuPOM } from "../../modules/POM/headerAndFooterPOM/tekaDropDownMenuPOM";
+import { syskorDropDownMenuPOM } from "../../modules/POM/headerAndFooterPOM/syskorDropDownMenuPOM";
+import { electroluxPlocePageBL } from "../../modules/BL/electroluxBL/electroluxPlocePageBL";
+
+describe("Testing blanco drink.filter page UI", () => {
+  before(() => {
+    cy.visit(`${Cypress.env("baseUrl")}`);
+  });
+
+  it("Assert all the elements on the blanco drink.filter page", () => {
+    electroluxPlocePageBL.assertElectroluxPlocePage();
+  });
+
+  after(() => {
+    headerAndFooterBL.assertHeaderAndFooterElemets();
+    blancoDropDownMenuPOM.assertBlancoDropdownMenu();
+    electroluxDropDownMenuPOM.assertElectroluxDropDownMenu();
+    tekaDropDownMenuPOM.assertTekaDropDownMenu();
+    syskorDropDownMenuPOM.assertSyskorDropDownMenu();
+  });
+});
---
+++ cypress/modules/POM/electroluxPOM/electroluxPlocePagePOM.js
@@ -0,0 +1,54 @@
+class ElectroluxPlocePagePOM {
+  get title() {
+    return cy.get("h1").contains("ELECTROLUX - Ploče");
+  }
+  get artikliNaStanjuFilter() {
+    return cy.get("#stanje_div");
+  }
+  get stanjeCheckbox() {
+    return cy.get("input[id='stanje']");
+  }
+  get izdvajamoFilter() {
+    return cy.get("#izdvojeni_div");
+  }
+  get vrstaPloceFilter() {
+    return cy.get("div[id='VRSTA PLOČE-list-1']");
+  }
+  get uranjanjeUMermerFilter() {
+    return cy.get("div[id='URANJANJE U MERMER-list-1']");
+  }
+  get bojaUredjajaFilter() {
+    return cy.get("div[id='BOJA UREĐAJA-list-1']");
+  }
+  get upravljanjeFilter() {
+    return cy.get("div[id='UPRAVLJANJE-list-1']");
+  }
+  get sirinaUredjajaFilter() {
+    return cy.get("div[id='ŠIRINA UREĐAJA-list-1']");
+  }
+  get sortirajDropDownMenu() {
+    return cy.get("#view-option-sort");
+  }
+  get poNazivuA_Zoption() {
+    return cy.get("select").select("Po nazivu A-Z");
+  }
+  get poNazivuZ_Aoption() {
+    return cy.contains("Po nazivu Z-A");
+  }
+  get poCeniRastuceOption() {
+    return cy.contains("Po ceni rastuće");
+  }
+  get poCeniOpadajućeOption() {
+    return cy.contains("Po ceni opadajuće");
+  }
+  get poUnosuOpadajuceOption() {
+    return cy.contains("Po unosu opadajuće");
+  }
+  get ponistiBtn() {
+    return cy.contains("Poništi");
+  }
+  get filteri() {
+    return cy.get("form[id='filteri']");
+  }
+}
+export const electroluxPlocePagePOM = new ElectroluxPlocePagePOM();
